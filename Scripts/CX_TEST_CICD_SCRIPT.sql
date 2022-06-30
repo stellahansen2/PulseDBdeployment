c683693bd52e5920627518f26861513b23046502
stellahansen2 <106402943+stellahansen2@users.noreply.github.com>
2022-06-30 12:51:56 -0400

    Update CX_TEST_CICD_SCRIPT.sql

--- Scripts/CX_TEST_CICD_SCRIPT.sql
+++ Scripts/CX_TEST_CICD_SCRIPT.sql
@@ -9,14 +9,5 @@ CREATE TABLE "GPC"."CX_TEST_CICD_SCRIPT"
 insert into "GPC"."CX_TEST_CICD_SCRIPT" values (13, 'stella', 'changed');
 insert into "GPC"."CX_TEST_CICD_SCRIPT" values (12, 'stella1', 'other changed');
 
-create or replace PROCEDURE secure_dml
-IS
-BEGIN
-  IF TO_CHAR (SYSDATE, 'HH24:MI') NOT BETWEEN '08:00' AND '18:00'
-        OR TO_CHAR (SYSDATE, 'DY') IN ('SAT', 'SUN') THEN
-	RAISE_APPLICATION_ERROR (-20205,
-		'You may only make changes during normal office hours');
-  END IF;
-END secure_dml;
 
-EXEC secure_dml;
+EXEC @../Procedures/secure_dml;
