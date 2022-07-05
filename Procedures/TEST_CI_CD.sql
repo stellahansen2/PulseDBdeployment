020632f9151a8c42444735978b0d36f801254750
stellahansen2 <106402943+stellahansen2@users.noreply.github.com>
2022-07-05 12:48:37 -0400

    Update and rename test_sp.sql to TEST_CI_CD.sql

---
+++ Procedures/TEST_CI_CD.sql
@@ -0,0 +1,25 @@
+create or replace PROCEDURE TEST_CI_CD AS 
+
+cursor csr_test is
+  select name,TABLECHANGED from CX_TEST_CICD_SCRIPT;
+
+BEGIN
+
+FOR csr_test_v IN csr_test LOOP
+    
+          begin
+            update CX_TEST_CICD_SCRIPT
+              set  name = CONCAT(name, '_changed')
+                 
+            where TABLECHANGED = 'changed';
+              
+          EXCEPTION when OTHERS then
+            null;  
+          end;  
+          
+    end LOOP;
+    
+    commit;
+
+  
+END TEST_CI_CD;
