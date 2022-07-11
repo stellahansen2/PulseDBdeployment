3b1a998c7950c76ad403cfa2e6e8a074942b8eaf
stellahansen2 <106402943+stellahansen2@users.noreply.github.com>
2022-07-11 08:27:34 -0400

    Update CX_TEST_CICD_SCRIPT.sql

--- Scripts/CX_TEST_CICD_SCRIPT.sql
+++ Scripts/CX_TEST_CICD_SCRIPT.sql
@@ -10,4 +10,4 @@ insert into "GPC"."CX_TEST_CICD_SCRIPT" values (13, 'Stella', 'changed');
 insert into "GPC"."CX_TEST_CICD_SCRIPT" values (12, 'stella1', 'not Changed');
 @../Procedures/TEST_CI_CD.sql;
 
-//added this, and this, and this
+//added this, and this, and this, change
