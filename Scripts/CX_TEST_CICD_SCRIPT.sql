c3c78c0b0254f10a2d7f380820fd9c7dbc7db4f4
Stella Hansen <458537@ac02zc4tzlvdq.genpt.net>
2022-07-12 15:57:34 -0400

    l

--- Scripts/CX_TEST_CICD_SCRIPT.sql
+++ Scripts/CX_TEST_CICD_SCRIPT.sql
@@ -9,8 +9,4 @@ CREATE TABLE "GPC"."CX_TEST_CICD_SCRIPT"
 insert into "GPC"."CX_TEST_CICD_SCRIPT" values (13, 'Stella', 'changed');
 insert into "GPC"."CX_TEST_CICD_SCRIPT" values (12, 'stella1', 'not Changed');
 @Procedures/TEST_CI_CD.sql;
-@Procedures/TEST_CI_CD.sql;
-@Procedures/TEST_CI_CD.sql;
-@Procedures/TEST_CI_CD.sql;
-@Procedures/TEST_CI_CD.sql;
 
